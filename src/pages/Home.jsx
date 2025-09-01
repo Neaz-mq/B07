b212d6752e18aec029e4ed2608f5cf4a020c3957
Neaz-mq <neazmorshed666@gmail.com>
2025-09-01 16:48:20 +0600

    Home Page working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import HomeBottomText from '../components/home/HomeBottomText'
+import HomeHeroText from '../components/home/HomeHeroText'
+import Video from '../components/home/Video'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='text-white'>
+      <div className='h-screen w-screen fixed'>
+        <Video />
+      </div>
+      <div className='h-screen w-screen relative pb-5 overflow-hidden flex flex-col justify-between'>
+        <HomeHeroText />
+        <HomeBottomText />
+      </div>
+    </div>
   )
 }
 
